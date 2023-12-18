003e3d35ecef1c606123aef7f5c379e9cce417b8
Mishka Spun <mishka.spun@yandex.ru>
2023-12-18 22:41:27 +0500

    для Димы

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,18 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const container = document.querySelector('.content');
+const placesList = container.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+const buttonDelete = container.querySelector('.card__delete-button');
+
+const createCard = (initialCards) =>  {
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImage = card.querySelector('.card__image');
+    cardImage.src = initialCards[i].link;
+    cardImage.alt = initialCards[i].name;
+  
+    card.querySelector('.card__title').textContent = initialCards.name;
+    
+    return card;
+  };
